4372ff08810950af8bc1c5a2b258051d270cc3c5
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-09-19 20:18:29 +0900

    add

--- coding_test/step14/1620.py
+++ coding_test/step14/1620.py
@@ -1,5 +1,30 @@
 #나는야 포켓몬 마스터 이다솜
 
-#(문제 김 주의;;)
+import sys
+input = sys.stdin.readline
 
+N, M = map(int, input().split())
+
+
+NumToName = {}
+NameToNum = {}
+
+for i in range(1, N + 1):
+  name = input().strip()
+  NumToName[i] = name
+  NameToNum[name] = i
+
+for _ in range(M):
+  query = input().strip()
+  if query.isdigit():
+    print(NumToName(int[query]))
+  else:
+    print(NameToNum[query])
+
+# is digit()
+#해당 문자열이 숫자로만 이루어져 있는지 확인.
+    #만약 숫자로만 이루어져 있다면?
+    #숫자를 이름으로 변환
+
+    #그렇지 않고 문자가 포함되어 있다면? 이름을 숫자로 변환
 
