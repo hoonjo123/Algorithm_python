3d40fd804eb201f6252bbff5647e64e124fe85db
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-10-30 14:40:30 +0900

    2346 풍선 터뜨리기

--- coding_test/step16/2346.py
+++ coding_test/step16/2346.py
@@ -1,2 +1,23 @@
 #풍선 터뜨리기
+def balloonGame(N,papers):
+  ballons = list(range(1, N+1))
+  result = []
+  idx = 0
 
+  for _ in range(N):
+    result.append(ballons.pop(idx))
+    if not ballons:
+      break
+    move = papers.pop(idx)
+    if move > 0:
+      idx = (idx + (move -1)) % len(ballons)
+    else:
+      idx = (idx + move) % len(ballons)
+
+  return result
+
+
+N = int(input())
+papers = list(map(int,input().split()))
+print(" ".join(map(str,balloonGame(N,papers))))
+  
