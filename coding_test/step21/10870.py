b89d004749fac9eade60d851a4c80c7eca1f0a40
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-11-04 14:45:34 +0900

    피보나치 재귀

--- coding_test/step21/10870.py
+++ coding_test/step21/10870.py
@@ -1,2 +1,10 @@
 #피보나치 수 5
 #피보나치 수 역시 단순 for문으로도 구할 수 있지만, 학습을 위해 재귀를 써 봅시다.
+
+def fibonacci(n):
+  if n <= 1:
+    return n
+  return fibonacci(n-1) + fibonacci(n-2)
+
+n = int(input())
+print(fibonacci(n))
