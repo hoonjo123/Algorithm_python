45afe637858a7135f46f762ab317ebc64c46e445
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-11-04 14:38:28 +0900

    재귀함수로 팩토리얼 풀기

--- coding_test/step21/27433.py
+++ coding_test/step21/27433.py
@@ -1,2 +1,13 @@
 #팩토리얼 2
 #팩토리얼은 단순 for문으로도 구할 수 있지만, 학습을 위해 재귀를 써 봅시다
+
+def factorial(N):
+  if N < 2:
+    return 1
+  else:
+    return N * factorial(N - 1)
+  
+
+N = int(input())
+  
+print(factorial(N))
