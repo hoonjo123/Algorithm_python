0bdce08bb8afd7f9c235c331a160661ff794198e
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-11-05 14:14:39 +0900

    별찍기

--- coding_test/step21/2447.py
+++ coding_test/step21/2447.py
@@ -1,2 +1,24 @@
 #별직기 - 10 (골드5)
 #재귀적인 패턴을 재귀함수로 찍는 문제
+def drawStars(n):
+  if n == 3:
+    return ["***","* *","***"]
+
+
+  prev = drawStars(n//3)
+  stars = []
+
+
+  for i in prev:
+    stars.append(i * 3)
+  for i in prev:
+    stars.append(i + " " * (n//3) + i)
+  for i in prev:
+    stars.append(i * 3)
+
+  return stars
+
+
+n = int(input())
+result = drawStars(n)
+print("\n".join(result))
