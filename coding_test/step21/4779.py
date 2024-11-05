dbc202536fda292df00e3ef9449fa830d0f73b03
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-11-05 14:03:48 +0900

    칸토어집합

--- coding_test/step21/4779.py
+++ coding_test/step21/4779.py
@@ -1,1 +1,14 @@
 #칸토어 집합
+import sys
+
+def kantor(n):
+  if n == 0:
+    return '-'
+  
+  pre = kantor(n-1)
+  remove = ' ' * len(pre)
+  return pre + remove + pre
+
+for line in sys.stdin:
+  n = int(line)
+  print(kantor(n))
