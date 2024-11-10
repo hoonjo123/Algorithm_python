657125345e910d1df91c91d76dd39105d0768a1a
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-11-10 17:25:17 +0900

    1260.

---
+++ coding_test/BFS/2606.py
@@ -0,0 +1,26 @@
+#바이러스
+
+def dfs(c):
+  global ans
+
+  ans += 1
+  v[c] = 1
+
+#c와 연결된 노드 모두 처리
+  for n in adj[c]:
+    if not v[n]:
+      dfs(n)
+
+N = int(input())
+M = int(input())
+adj = [[] for _ in range(N+1)]
+for _ in range(M):
+  s, e = map(int, input().split())
+  adj[s].append(e)
+  adj[e].append(s)
+
+
+ans = 0
+v = [0] * (N+1)
+dfs(1)
+print(ans -1)
