300c56e90b037ff166eb9f0192f05b027d660ced
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-11-10 18:28:03 +0900

    2667

--- coding_test/BFS/2667.py
+++ coding_test/BFS/2667.py
@@ -1,1 +1,38 @@
 #단지번호 붙이기
+from collections import deque
+
+def bfs(si, sj):
+  q = deque()
+  q.append((si, sj))
+  # v[si][sj] = 1
+  arr[si][sj] = 0
+  cnt = 1
+
+  directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+  while q:
+    ci,cj = q.popleft()
+    for di,dj in directions:
+      ni,nj = ci+di, cj+dj
+      if 0<=ni<N and 0<=nj<N and arr[ni][nj] == 1:
+        q.append((ni,nj))
+        arr[ni][nj] = 0
+        cnt += 1
+  
+  return cnt
+
+
+N = int(input())
+arr = [list(map(int,input().strip())) for _ in range(N)]
+# v = [[0]*N for _ in range(N)]
+ans = []
+
+for i in range(N):
+  for j in range(N):
+    if arr[i][j] == 1:
+      ans.append(bfs(i,j))
+
+
+ans.sort()
+print(len(ans))
+print(*ans, sep='\n')
