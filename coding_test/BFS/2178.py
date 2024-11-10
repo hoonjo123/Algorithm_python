e9a08204e01c803c1c0297ca71a2a8fcfc97b352
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-11-10 17:12:04 +0900

    dfs,bfs

--- coding_test/BFS/2178.py
+++ coding_test/BFS/2178.py
@@ -1,6 +1,10 @@
 #미로탐색
 import sys
 
+#si, sj = start i, start j
+#ei, ej = end i , end j
+#ni, nj = next i, next j
+#ci, cj + current i, current j
 
 
 def bfs(si, sj, ei, ej):
@@ -16,11 +20,14 @@ def bfs(si, sj, ei, ej):
       return v[ei][ej]
 
 
+#전형적인 bfs 패턴, 4방향 탐색하는 좌표
+    
     for di,dj in ((-1,0), (1,0), (0,-1),(0,1)):
       ni, nj = ci+di, cj+dj
+      #조건에 맞으면: arr == 1(길어거나) , v == 0(방문하지 않았거나)
       if 0<=ni<N and 0<=nj<M and arr[ni][nj]==1 and v[ni][nj] == 0:
         q.append((ni,nj))
-        v[ni][nj] = v[ci][cj]+1
+        v[ni][nj] = v[ci][cj]+1 
 
 
 N,M = map(int,input().split())
